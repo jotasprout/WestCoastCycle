d7af35ee9ab62418c0cacc71539bd92517d90e15
JotaSprout <jotasprout@gmail.com>
2018-12-01 16:02:17 -0500

    Add ServiceWorker

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-/* something will go here */
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js")
+        .then(function(registration) {
+            console.log("This page now controlled by your awesome ServiceWorker with scope: ", registration.scope);
+        }).catch(function(err) {
+            console.log("Service worker registration failed: ", err);
+        });
+}
